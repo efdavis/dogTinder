8e645b5707e750a7998c00bf5b226039bea2ea68
Scott Moschella <scottmosch@gmail.com>
2017-04-26 16:03:22 -0700

    Add body-parser to request handler

--- request-handler.js
+++ request-handler.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('./public'));
+app.use(bodyParser.json());
 
 app.get('/', function(request, response){
   response.sendfile(path.resolve(__dirname, "./public/index.html"));
