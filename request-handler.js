3277d9b693a9593dfbdf62733c33eed51d70a076
Scott Moschella <scottmosch@gmail.com>
2017-04-26 17:46:03 -0700

    Add api endpoint to request handler

--- request-handler.js
+++ request-handler.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var petFinderFetch = require('./utils/petfinderHelper')
+var request = require('request');
+
 var app = express();
 
 app.use(express.static('./public'));
@@ -9,5 +12,15 @@ app.use(bodyParser.json());
 app.get('/', function(request, response){
   response.sendfile(path.resolve(__dirname, "./public/index.html"));
 });
+
+app.get('/api', function(req, res){
+  // connect to API and get matching dogs
+  // connect to DB and get matching dogs
+  petFinderFetch(req.query, function(dogs){
+    res.send(dogs);
+  })
+
+
+})
 
 module.exports = app;
