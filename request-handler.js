1ab9b9def860d5408648897b72968600ee87aabc
Scott Moschella <scottmosch@gmail.com>
2017-05-05 12:56:06 -0700

    Save FB ID to user session and console log user at the '/'

--- request-handler.js
+++ request-handler.js
@@ -1,21 +1,25 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var petFinderFetch = require('./utils/petfinderHelper')
+var petFinderFetch = require('./utils/petfinderHelper');
 var request = require('request');
-var passport = require('./utils/fbPassportHelper')
+var passport = require('./utils/fbPassportHelper');
+var session = require('express-session');
 
 var app = express();
 
 app.use(express.static('./public'));
 app.use(bodyParser.json());
-
+app.use(session({ secret: process.env.SESSION_SECRET, saveUninitialized: true, resave: true, name: 'dogbiscuit' }));
 // set up passport for FB Auth on Express
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get('/', function(request, response){
-  response.sendfile(path.resolve(__dirname, "./public/index.html"));
+  if(request.session.user) {
+    console.log(request.session.user.displayName + ' is logged in with FB ID: ' + request.session.user.id)
+  }
+  response.sendFile(path.resolve(__dirname, "./public/_index.html"));
 });
 
 // signup/login
@@ -30,9 +34,13 @@ app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
     // Successful authentication, redirect home.
+    req.session.user = req.user
+    // find or add user
+    // user id is req.user.id
+    // user's name is req.user.displayName
+    // res.send(req.user);
     res.redirect('/');
-    // console.log(req.user);
-    // find or add
+    
   });
 
 app.get('/dog-tinder-api', function(req, res){
