91839cdf9a223a55935f5d2bdd2f20b50dde39a5
Yujin Chung <yujin0chung@gmail.com>
2017-05-05 15:12:14 -0700

    Adds font files

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -47,24 +47,39 @@ class NavBar extends React.Component {
                 </FormGroup>
                 <Button type="submit" onClick={() => this.props.submitQuery(this.state)}>Submit</Button>
               </Navbar.Form>
+
+              <div className="container">
+
+                <select>
+                  <option value="test">Female</option>
+                  <option value="test2">Male</option>
+                </select>
+
+                <select>
+                  <option value="test">Young</option>
+                  <option value="test2">Adult</option>
+                  <option value="test2">Senior</option>
+                </select>
+
+              </div>
             
-              <DropdownButton  title={'Age'} id={`dropdown-basic-${1}`} onSelect={() => this.props.submitQuery(this.state)}>
+              {/*<DropdownButton  title={'Age'} id={`dropdown-basic-${1}`}>
                 <MenuItem eventKey="1" value={this.state.age} onSelect={(e) => this.setState({age: e.target.value})}>Young</MenuItem>
                 <MenuItem eventKey="2" value={this.state.age} onSelect={(e) => this.setState({age: e.target.value})}>Adult</MenuItem>
                 <MenuItem eventKey="3" value={this.state.age} onSelect={(e) => this.setState({age: e.target.value})}>Senior</MenuItem>
               </DropdownButton>
 
-              <DropdownButton  title={'Sex'} id={`dropdown-basic-${1}`} onSelect={() => this.props.submitQuery(this.state)}>
+              <DropdownButton  title={'Sex'} id={`dropdown-basic-${1}`}>
                 <MenuItem eventKey="1" value={this.state.sex} onSelect={(e) => this.setState({sex: e.target.value})}>Female</MenuItem>
                 <MenuItem eventKey="2" value={this.state.sex} onSelect={(e) => this.setState({sex: e.target.value})}>Male</MenuItem>
               </DropdownButton>
               
-              <DropdownButton  title={'Breed'} id={`dropdown-basic-${1}`} onSelect={() => this.props.submitQuery(this.state)}>
+              <DropdownButton  title={'Breed'} id={`dropdown-basic-${1}`} >
                 <MenuItem eventKey="1" >Female</MenuItem>
-              </DropdownButton>
+              </DropdownButton>*/}
               
              
-              <div className="dropdown clearfix"> <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"> Dropdown <span className="caret"></span> </button> <ul className="dropdown-menu" aria-labelledby="dropdownMenu1"> <li><a href="#">Action</a></li> <li><a href="#">Another action</a></li> <li><a href="#">Something else here</a></li> </ul> </div>
+              {/*<div className="dropdown clearfix"> <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"> Dropdown <span className="caret"></span> </button> <ul className="dropdown-menu" aria-labelledby="dropdownMenu1"> <li><a href="#">Action</a></li> <li><a href="#">Another action</a></li> <li><a href="#">Something else here</a></li> </ul> </div>*/}
 
 
 
