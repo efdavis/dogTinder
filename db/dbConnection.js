37f0a8b3f8327da54e30ba3dd09a1bf5c8fb1889
joshuastagner <joshuastagner@gmail.com>
2017-05-08 14:34:57 -0700

    fix sequelize connection code

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_DATABASENAME , process.env.DB_PASSWORD, process.env.DB_USERNAME , process.env.DB_PASSWORD, {host: process.env.DB_LOCALHOST, dialect: 'postgres', underscored: true});
+const sequelize = new Sequelize(process.env.DB_DATABASENAME , process.env.DB_USERNAME, process.env.DB_PASSWORD, {host: process.env.DB_HOST, dialect: 'postgres', underscored: true});
 
 const User = sequelize.define('user', {
   id: {
