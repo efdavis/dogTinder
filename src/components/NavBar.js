224c5c9a25f3e55a7e8994e0c9d9cb071647bb80
Scott Moschella <scottmosch@gmail.com>
2017-05-10 11:54:35 -0700

    Update NavBar component to have async state set

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,11 +13,17 @@ class NavBar extends React.Component {
       sex: ''
     };
   this.handleSelect = this.handleSelect.bind(this);
+  this.handleZipChange = this.handleZipChange.bind(this);
   }
  
 
-  handleSelect() {
-    this.props.submitQuery(this.state);
+  handleSelect(event) {
+    event.preventDefault();
+    const name = event.target.name;
+    this.setState({[name]: event.target.value}, () => {
+      this.props.submitQuery(this.state);
+    });
+    // this.props.submitQuery(this.state);
   }
 
   validate(zipcode){
@@ -28,37 +34,44 @@ class NavBar extends React.Component {
     }
   }
 
+  handleZipChange(e){
+    if(e.target.value.length === 5 && Number.isInteger(parseInt(e.target.value))) {
+      this.setState({zipcode: e.target.value}, () => {
+        this.props.submitQuery(this.state);
+      })
+    }
+  }
+
 
   render() {
-    console.log('navbar this.state:', this.state)
-
     return (
       <div style={{display: 'flex', flexDirection: 'row', alignItems:'center', justifyContent: 'flex-start'}}>
           
+        <form onSubmit={this.handleSelect}>
           <div className="zipcode">
-            <form onSubmit={this.handleSelect}>
+            {/*<form onSubmit={this.handleSelect}>*/}
               <label style={{display: 'flex', flexDirection: 'row', fontWeight: 400, height: '32px', backgroundColor: 'white', borderColor: '#ccc', marginRight: '17px'}}>
-                <input placeholder="07470" value={this.state.zipcode} onChange={(e) => this.setState({zipcode: e.target.value})} />                
+                <input placeholder="07470" onChange={this.handleZipChange} />                
               </label>
-            </form>
+            {/*</form>*/}
           </div>
               
 
           <div className="formBox" style={{display: 'flex', flexDirection: 'row'}}>
          
-            <form id="selectGender" >
+            {/*<form id="selectGender" >*/}
               <label style={{display: 'flex', flexDirection: 'row', fontWeight: 400, height: '32px', backgroundColor: 'white', borderColor: '#ccc', marginRight: '17px'}}>
-                <select value={this.state.sex} onChange={(e) => {this.setState({sex: e.target.value}); this.handleSelect()}} style={{ backgroundColor: 'white'}}>
+                <select name="sex" onChange={this.handleSelect} style={{ backgroundColor: 'white'}}>
                   <option defaultValue="gender">Gender</option>
                   <option value="F">Female</option>
                   <option value="M">Male</option>
                 </select>
               </label>
-            </form>
+            {/*</form>*/}
 
-            <form id="selectAge">
+            {/*<form id="selectAge">*/}
               <label style={{display: 'flex', flexDirection: 'row', fontWeight: 400, height: '32px', backgroundColor: 'white', borderColor: '#ccc', marginRight: '17px'}}>
-                <select value={this.state.age} onChange={(e) => {this.setState({age: e.target.value}); this.handleSelect()}} style={{ backgroundColor: 'white'}}>
+                <select name="age" onChange={this.handleSelect} style={{ backgroundColor: 'white'}}>
                   <option defaultValue="age">Age</option>
                   <option value="Baby">Baby</option>
                   <option value="Young">Young</option>
@@ -66,17 +79,17 @@ class NavBar extends React.Component {
                   <option value="Senior">Senior</option>
                 </select>
               </label>
-            </form>
+            {/*</form>*/}
 
-            <form id="selectBreed">
+            {/*<form id="selectBreed">*/}
               <label style={{display: 'flex', flexDirection: 'row', fontWeight: 400, height: '32px', backgroundColor: 'white', borderColor: '#ccc', marginRight: '17px'}}>
-                <select value={this.state.breed} onChange={(e) => {this.setState({breed: e.target.value}); this.handleSelect()}} style={{ backgroundColor: 'white'}}>
+                <select name="breed" onChange={this.handleSelect} style={{ backgroundColor: 'white'}}>
                   <option defaultValue="breed">Breed</option>
-                  {breeds.map(dog =>  <option value={dog.$t} onSelect={console.log('breed has been selected!')}>{dog.$t}</option> )}
+                  {breeds.map(dog =>  <option value={dog.$t} >{dog.$t}</option> )}
                 </select>
               </label>
-            </form>
             </div>            
+          </form>
           </div>
       
     );
