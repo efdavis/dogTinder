9427dd2787fd071091db771ab8c43669b9498575
Yujin Chung <yujin0chung@gmail.com>
2017-05-09 18:08:46 -0700

    Creates profileBox with close button

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -26,22 +26,27 @@ class Kennel extends React.Component {
     })
   }
 
+
+
   render(){
     return (
     <div>
-      <h1 className="page-header">My Kennel</h1>
+    <h1 className="page-header">My Kennel</h1>
+    <div style={{display: 'flex'}}>
         <ul className="media-list col-md-6">{this.props.animalList.map((dog) => {
-          return <li className="media dog-box" key={dog.id.$t} style={{display: 'flex'}}>
+          return <li className="media dog-box" key={dog.id.$t} style={{display: 'flex', justifyContent: 'flex-start'}}>
                     <div className="media-left">
                       <img className="media-object img-rounded" src={dog.media.photos.photo[0]} width="50px" height="50px" />
                     </div>
-                    <h5 className="dogName" onClick={() => {this.clickDogName(); this.showProfile(dog)}}>{dog.name.$t}</h5>
+                    <h5 className="dogName" onClick={() => {this.clickDogName(); this.showProfile(dog)}}> {dog.name.$t} / {Array.isArray(dog.breeds.breed)? <span>Mixed Breed</span> : dog.breeds.breed.$t}  / {dog.age.$t} / {dog.sex.$t}</h5>
                     {/*{this.state.clicked ? <div>{this.state.selectedDog.name.$t}</div> : <div></div>}*/}
                     {/*{this.state.clicked ? <KennelDogProfile name={dog.name.$t} image={dog.media.photos.photo[0]}breed={dog.breeds.breed.$t} age={dog.age.$t} sex={dog.sex.$t} description={dog.description.$t}/> : <div></div>}*/}
                 </li>
         })}</ul>
         {this.state.clicked ? <KennelDogProfile dog={this.state.selectedDog}/> : <div></div> }    
-    </div>);
+    </div>
+    </div>
+    );
         
   }
 }
--- src/components/KennelDogProfile.js
+++ src/components/KennelDogProfile.js
@@ -3,16 +3,34 @@ import React from 'react';
 class KennelDogProfile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {clicked: false};
   }
+
+  closeProfile() {
+    this.setState({clicked: !this.state.clicked})
+  }
+
   render() {
     console.log('this.props from kenneldogprofile:', this.props.dog)
-    // const dog = this.props.dog;
+    const dog = this.props.dog;
+    if (this.state.clicked) {
+        return (<div></div>);
+    }
+    else {
     return (
-      <div style={{display: 'flex', flexDirection: 'row'}}>
-        <h3>{this.props.dog.name.$t}</h3>
-        <img src={this.props.dog.media.photos.photo[0]} />
+    <div>
+     <h3>{dog.name.$t}</h3>
+      <div className="doggyProfile" style={{display: 'flex', flexDirection: 'row', alignSelf: 'auto', borderStyle: 'solid'}}>
+        {/*<div><h3>{this.props.dog.name.$t}</h3></div>*/}
+        <img src={dog.media.photos.photo[0]} style={{width: '50%', height: '40%' }}/>
+        <div className="profile-description">{dog.description.$t}</div>
+        <button type="button" className="btn btn default btn-sm" onClick={() => this.closeProfile()}>
+          <span className="glyphicon glyphicon-remove" aria-hidden="true"></span>
+        </button>
       </div>
+    </div>
     );
+  }
   }
 }
 
