c5c5380d5e452347e3af6bad3b79321941c10577
Scott Moschella <scottmosch@gmail.com>
2017-05-11 10:53:05 -0700

    Make Facebook button smaller

--- src/components/FacebookLogin.js
+++ src/components/FacebookLogin.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const FacebookLogin = (props) => {
 
-  return (<a className="btn btn-block btn-social btn-facebook" href="/auth/facebook"><i className="fa fa-facebook"></i>Sign in with Facebook</a>);
+  return (<a className="btn btn-block btn-social btn-facebook btn-small" href="/auth/facebook"><i className="fa fa-facebook"></i>Sign in with Facebook</a>);
 }
 
 module.exports = FacebookLogin;
